80d4f8a6dd111d591da1ece2cdcc158ce2da8899
ederxdias <ederxdias98@gmail.com>
2025-05-23 14:57:58 -0300

    sexta

--- src/ChessGUI.java
+++ src/ChessGUI.java
@@ -12,7 +12,10 @@ public class ChessGUI extends JFrame {
         setTitle("Xadrez Swing");
         setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(new chessPainel());
+        this.setLayout(new BorderLayout());
+        chessPainel painel = new chessPainel();
+
+        this.add(painel,BorderLayout.CENTER);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         
@@ -22,13 +25,13 @@ public class ChessGUI extends JFrame {
     }
     public ImageIcon escalaImagemIcon(ImageIcon icon){
         Image img = icon.getImage(); // Obtém a imagem
-        Image novaImg = img.getScaledInstance(50, 50, Image.SCALE_SMOOTH); // Redimensiona a imagem
+        Image novaImg = img.getScaledInstance(15, 15, Image.SCALE_SMOOTH); // Redimensiona a imagem
         return new ImageIcon(novaImg);
    }
 
     public class chessPainel extends JPanel{
         private static final int BOARD_SIZE = 8;
-        private static final int SQUARE_SIZE = 75;
+      
         ImageIcon peao_branco;
         ImageIcon peao_preto;
     
@@ -36,8 +39,8 @@ public class ChessGUI extends JFrame {
             peao_branco = new ImageIcon("./imgs/white_pawn.png");
             peao_preto = new ImageIcon("./imgs/black_pawn.png");
     
-            peao_branco = escalaImagemIcon(peao_branco);
-            peao_preto = escalaImagemIcon(peao_preto);
+            // peao_branco = escalaImagemIcon(peao_branco);
+            // peao_preto = escalaImagemIcon(peao_preto);
 
 
         }
@@ -45,7 +48,10 @@ public class ChessGUI extends JFrame {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-    
+            int larguraPainel = getWidth();
+            int alturaPainel = getHeight();
+            int squareSize = Math.min(larguraPainel, alturaPainel) / BOARD_SIZE;
+            
             // Percorre linhas e colunas
             for (int linha = 0; linha < BOARD_SIZE; linha++) {
                 for (int coluna = 0; coluna < BOARD_SIZE; coluna++) {
@@ -58,9 +64,20 @@ public class ChessGUI extends JFrame {
                     }
     
                     // Desenha o quadrado (casa)
-                    int x = coluna * SQUARE_SIZE;
-                    int y = linha * SQUARE_SIZE;
-                    g.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+                    int offsetX = (larguraPainel - (squareSize * BOARD_SIZE)) / 2;
+                    int offsetY = (alturaPainel - (squareSize * BOARD_SIZE)) / 2;
+                    int squareSizePeca = squareSize/2;
+
+                    int offsetXp = (squareSize - (squareSizePeca)) / 2;
+                    int offsetYp =(squareSize - (squareSizePeca)) / 2;
+
+                    int x = offsetX + coluna * squareSize;
+                    int y = offsetY + linha * squareSize;
+                    int xp = x + offsetXp;
+                    int yp = y + offsetYp;
+                    g.fillRect(x, y, squareSize, squareSize);
+                    
+                    g.drawImage(peao_branco.getImage(), xp, yp, squareSizePeca, squareSizePeca, this);
                 }
             }
         }
